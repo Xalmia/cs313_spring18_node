8f5852c76f3117e492806cf103bd22cfcd1684df
Jesse Scott <xalmia@gmail.com>
2018-06-21 16:12:28 -0600

    Adding npm things

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const path = require('path');
 const PORT = process.env.PORT || 5000
 
 var app = express();
 
 app.get('/', (res, req) => {
-  res.sendFile(path.join(__dirname, 'public', 'mail'));
+  req.sendFile(path.join(__dirname, 'public', 'mail.html'));
 })
 
+app.get('/getPackage', (res, req) => {
+  req.send("Hello there.");
+})
+
+app.listen(5000, (res, req) => {
+  console.log("Listening on port 5000");
+})
 /*
 express()
   .use(express.static(path.join(__dirname, 'public')))
