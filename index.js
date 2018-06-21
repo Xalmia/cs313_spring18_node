f24a62a79f5b93962d2876d4b2225b61fa16a4b5
Jesse Scott <xalmia@gmail.com>
2018-06-21 16:00:48 -0600

    trying things

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+var app = express();
+
+app.get('/', (res, req) => {
+  res.sendFile(path.join(__dirname, 'public', 'mail'));
+})
+
+/*
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/public/mail', function(req, res) {
-    
-  })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))*/
